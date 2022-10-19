244b53e7de041cbbfc4c64e2fc7eb04cf95736f6
Pamela UMWALI <p.umwali@alustudent.com>
2022-10-19 19:24:40 +0200

    last one

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,9 +1,4 @@
  #!/usr/bin/python3
 def magic_calculation(a, b):
-    return (98 + (a ** b))
-<<<<<<< HEAD
-      
-
-=======
-                       
->>>>>>> 9fd98feda71367865ee6da2079191601b136c6a4
+    return (98 + (a ** b)) 
+       
