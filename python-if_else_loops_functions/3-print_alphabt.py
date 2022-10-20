ee64149a7b64771ab78a533ba0213aa464274e7e
Pamela UMWALI <p.umwali@alustudent.com>
2022-10-20 23:27:29 +0200

    added space

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
     if letter != ord('e') and letter != ord('q'):
-        print("{:c}".format(letter), end="")
+        print("{:c}".format(letter), end="")  
