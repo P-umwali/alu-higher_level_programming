159d8e469109847696cc7bed3668e21f1ad2bab3
Pamela UMWALI <p.umwali@alustudent.com>
2022-12-05 18:33:00 +0200

    first task

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+ #!/usr/bin/python3
+"""
+ a fuction that adsds 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+all about test driven development
