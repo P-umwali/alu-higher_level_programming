d7ae4a235f3fa90c14228e74d18aaa051c4c22c9
Pamela UMWALI <p.umwali@alustudent.com>
2022-12-08 18:08:21 +0200

    spaces

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
        """initialization of the attributes"""
+       
        super().__init__(id)
        self.width = width
        self.height = height
@@ -30,6 +31,7 @@ class Rectangle(Base):
 
         if value <= 0:
             raise ValueError("width must be > 0")
+        
         self.__width = value
 
     @property
@@ -53,20 +55,25 @@ class Rectangle(Base):
     @property
     def x(self):
         """getting the x"""
+      
         return self.__x
 
     @x.setter
     def x(self, value):
         """x"""
+        
         if type(value) != int:
             raise TypeError("x must be an integer")
+        
         if value < 0:
             raise ValueError("x must be >= 0")
+      
         self.__x = value
 
     @property
     def y(self):
         """ getting the y"""
+     
         return self.__y
 
     @y.setter
@@ -83,6 +90,7 @@ class Rectangle(Base):
 
     def area(self):
         """area of the rectangle"""
+      
         return self.width * self.height
 
     def display(self):
