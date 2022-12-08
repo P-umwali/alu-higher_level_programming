2a216ad7504f466103d4e242b97d33a0b6c42776
Pamela UMWALI <p.umwali@alustudent.com>
2022-12-08 22:05:18 +0200

    last last

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,14 +7,14 @@ from models.base import Base
 class Rectangle(Base):
     """Rectangle class inherites from base"""
 
-def __init__(self, width, height, x=0, y=0, id=None):
-    """initialization of the attributes"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+     """initialization of the attributes"""
 
-    super().__init__(id)
-    self.width = width
-    self.height = height
-    self.x = x
-    self.y = y
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
