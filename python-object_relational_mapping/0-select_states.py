c7cf1672c10ff4f4a6333760e40919e57af4e55d
Pamela UMWALI <p.umwali@alustudent.com>
2022-12-15 10:58:40 +0200

    last last

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""  A list of all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        charset="utf8")
+
+    ora = db.cursor()
+    ora.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in ora.fetchall():
+        print(row)
+    ora.close()
+    db.close()
---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A script that takes in an argument and displays all values in the \
+    states table."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db_get = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        db=argv[3])
+
+    ora = db_get.cursor()
+    ora.execute("SELECT * FROM states WHERE name = '{}' ORDER BY \
+        id ASC".format(argv[4]))
+
+    for row in ora.fetchall():
+        if row[1] == argv[4]:
+            print(row)
+    ora.close()
+    db_get.close()
