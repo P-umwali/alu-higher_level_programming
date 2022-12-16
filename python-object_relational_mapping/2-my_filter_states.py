9e2430b8dd7b3748dbd5a88b5ed9ab1226ab4bb7
Pamela UMWALI <p.umwali@alustudent.com>
2022-12-16 09:56:19 +0200

    new commits

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python3
-""" A script that takes in an argument and displays all values in the \
-    states table."""
+ #!/usr/bin/python3
+"""Lists all states from hbtn_0e_0_usa database that matches the arg."""
 
+import sys
 import MySQLdb
-from sys import argv
+
 
 if __name__ == "__main__":
-    db_get = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        password=argv[2],
-        db=argv[3])
+    my_connect = MySQLdb.connect(
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            host="localhost",
+            port=3306
+        )
 
-    ora = db_get.cursor()
-    ora.execute("SELECT * FROM states WHERE name = '{}' ORDER BY \
-        id ASC".format(argv[4]))
+    my_cursor = my_connect.cursor()
+    sql = """SELECT * FROM states \
+            WHERE  name LIKE BINARY '{}' \
+            ORDER BY id ASC""".format(sys.argv[4])
 
-    for row in ora.fetchall():
-        if row[1] == argv[4]:
-            print(row)
-    ora.close()
-    db_get.close()
+    my_cursor.execute(sql)
+    result = my_cursor.fetchall()
+
+    for i in result:
+        print(i)
+    my_connect.close()
