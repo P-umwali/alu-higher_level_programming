c5ccc94b7678d667218ae7ff26e83b2d61c05a40
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-08 11:52:44 +0200

    javaaaa

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const numArgv = process.argv.legnth 
-if (numArgv <3) {
-    console.log('No argument');
-} else if(numArgv === 3){
-    console.log('Argument found');
+const numArgv = process.argv.legnth;
+if (numArgv < 3) {
+  console.log('No argument');
+} else if (numArgv === 3) {
+  console.log('Argument found');
 } else {
-    console.log ('Arguments found');
+  console.log('Arguments found');
 }
