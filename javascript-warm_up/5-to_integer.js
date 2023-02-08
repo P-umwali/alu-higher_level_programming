e065c5021dd0f9cde948f91c202716940d205e40
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-08 15:34:18 +0200

    indentetion

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const num = parseInt(process.argv[2]);
 if (Number.isNaN(num)) {
   console.log('Not a number');
 } else {
-    console.log('My number: ' + num);
+  console.log('My number: ' + num);
 }
