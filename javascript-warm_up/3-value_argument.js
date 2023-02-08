faa54383276afd1a61096e9bd718a964f61bafe9
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-08 12:20:18 +0200

    new

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const argv = process.argv[2]
-if (argv === undefined){
-    console.log('No argument')
+const argv = process.argv[2];
+if (argv === undefined) {
+  console.log('No argument');
 }
