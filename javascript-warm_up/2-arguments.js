53de6e7a5631e8c4630213ab368480b3236314ef
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-08 10:51:44 +0200

    Javascript warm up tasks

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const numArgv = process.argv.legnth 
+if (numArgv <3) {
+    console.log("No argument");
+} else if(numArgv === 3){
+    console.log("Argument found");
+} else {
+    console.log ("Arguments found");
+}
---
+++ javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+Java script here i come
