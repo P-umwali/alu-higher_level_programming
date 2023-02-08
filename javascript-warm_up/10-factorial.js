26db6c74b68e1fd52af6012ef8f1fb84bd98fc61
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-08 12:46:22 +0200

    n

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = parseInt(process.argv[2]);
 function factorialize (num) {
-  if ((number.isNaN(num)) || (num === 1)) {
+  if ((Number.isNaN(num)) || (num === 1)) {
     return 1;
   }
   return factorialize(num - 1) * num;
