d3ff2bf00c69fbe42fe49688f67143d59f753c97
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-08 12:35:35 +0200

    new c

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const argv = process.argv[2];
 if (argv === undefined) {
   console.log('No argument');
-}
-=======
-const argv = process.argv[2]
-if (argv === undefined){
-    console.log('No argument')
 }
--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const firstArgv = process.argv[2];
-if (isNaN(firstArgv)){
+if (isNaN(firstArgv)) {
     console.log('Missing number of occurences');
 } else{
-    for (let i = 0; i< firstArgv; i++){
-        console.log('C is fun');
+    for (let i = 0; i < firstArgv; i++) {
+      console.log('C is fun');
     }
 }
