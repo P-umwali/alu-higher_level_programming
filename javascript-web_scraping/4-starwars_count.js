917200df0c0c9ccc2d54f43251dae282d98d0093
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-10 08:43:27 +0200

    s

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ req.get(url, (err, body) => {
   } else {
     let times = 0;
     for (const i of JSON.parse(body.body).results) {
-      const character = i.character;
+      const character = i.characters;
       for (const j of character) {
         if (j.includes('18')) {
           times += 1;
